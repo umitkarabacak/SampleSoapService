a6a59662a8066f828d05522dc31c89ff4f4301e8
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-02-02 16:06:59 +0300

    default api layer created.

---
+++ Api/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("information log ");
+
+            return Ok();
+        }
+    }
+}
